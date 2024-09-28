e51aede50093eaa88559a4ea5c5897d0ac8ca274
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-09-28 10:33:45 +0200

    Moving everything in place

---
+++ ggMatrixElements/functions.h
@@ -0,0 +1,18 @@
+#ifndef ggMatrixElements_functions_h
+#define ggMatrixElements_functions_h
+
+double ReB(double z);
+
+double ImB(double z);
+
+double ReT(double z);
+
+double ImT(double z);
+
+double ReF(double q, double a);
+
+double ReI(double z, double w);
+
+double ImI(double z, double w);
+
+#endif
