983de05df024c034f48b1e283c5c564e4489a02e
Laurent Forthomme <laurent.forthomme@cern.ch>
2025-04-30 13:21:40 +0200

    Using alphaEM evolution

--- src/Processes/GammaGammaToFF.cpp
+++ src/Processes/GammaGammaToFF.cpp
@@ -16,7 +16,9 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <CepGen/Modules/CouplingFactory.h>
 #include <CepGen/Physics/Constants.h>
+#include <CepGen/Physics/Coupling.h>
 #include <CepGen/Physics/PDG.h>
 
 #include "CepGenEPA/TwoPartonProcess.h"
@@ -28,15 +30,17 @@ class GammaGammaToFF : public epa::TwoPartonProcess {
 public:
   explicit GammaGammaToFF(const ParametersList& params)
       : epa::TwoPartonProcess(params),
+        alpha_em_(AlphaEMFactory::get().build(steer<ParametersList>("alphaEM"))),
         fermion_properties_(steer<ParticleProperties>("fermion")),
         min_w2_(std::pow(2. * fermion_properties_.mass, 2)),
-        prefactor_(4. * M_PI * constants::GEVM2_TO_PB * constants::ALPHA_EM * constants::ALPHA_EM *
-                   std::pow(fermion_properties_.integerCharge() / 3., 4) * fermion_properties_.colours) {}
+        prefactor_(4. * M_PI * constants::GEVM2_TO_PB * std::pow(fermion_properties_.integerCharge() / 3., 4) *
+                   fermion_properties_.colours) {}
 
   static ParametersDescription description() {
     auto desc = epa::TwoPartonProcess::description();
     desc.setDescription("Two-photon production of fermion pair");
     desc.add("fermion", 13);
+    desc.add("alphaEM", AlphaEMFactory::get().describeParameters("fixed"));
     return desc;
   }
 
@@ -48,11 +52,15 @@ public:
     const auto beta2 = 1. - min_w2_ / w2;
     if (beta2 < 0.)
       return 0.;
+    const auto alpha_em = alpha_em_->operator()(w);
     const auto beta = std::sqrt(beta2);
-    return prefactor_ / w2 * beta * (3. - beta2 * beta2) / (2 * beta) * std::log((1. + beta) / (1. - beta)) - 2 + beta2;
+    return prefactor_ * alpha_em * alpha_em / w2 * beta * (3. - beta2 * beta2) / (2 * beta) *
+               std::log((1. + beta) / (1. - beta)) -
+           2 + beta2;
   }
 
 private:
+  const std::unique_ptr<Coupling> alpha_em_;
   const ParticleProperties fermion_properties_;
   const double min_w2_;
   const double prefactor_;
